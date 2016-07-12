626138bf3d0d8995ea3c4359bf9e77bbbecf4537
Zoe D <zdormuth@gmail.com>
2016-07-12 07:56:16 -0500

    index_lab_complete

--- javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
@@ -37,7 +37,11 @@ public class TermCounter {
 	 */
 	public int size() {
         // TODO: fill this in.
-		return -1;
+		int count = 0;
+		for (Object value : map.values()) {
+			count += ((int)value);
+		}
+		return count;
 	}
 
 	/**
